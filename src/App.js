0f6562eb9a340ba161c30356dc3886e824ed6ebb
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-11-24 14:52:01 -0800

    added header and images

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import data from './assets/data/dogs.json'
+import Header from './snippets/Header.jsx'
 class App extends Component {
   render() {
+    const img = require('./assets/images/banner.jpg');
+    let images = data.dogs.map(dog =>  {
+      console.log(dog.image, 'dog')
+    return <img  src={dog.image} alt=""  />
+ });
     return (
       <div className="App">
         <header className="App-header">
-          <div>
-            <h1>Find a new Friend</h1>
-          </div>
+            <Header/>
         </header>
         <div className='container'>
           <div id='gallery'>
+            {images}
            </div>
         </div>
       </div>
---
+++ src/snippets/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+function Header () {
+   return (
+     <header class="global-header" role="banner">
+       <h1>Puppy image gallery</h1>
+
+      <blockquote class="header-quote">
+        <p>A dog will teach you unconditional love. If you can have that in your life, things won't be too bad.</p>
+      <cite>&mdash;Robert Wagner</cite>
+    </blockquote>
+  </header>)
+}
+export default Header
