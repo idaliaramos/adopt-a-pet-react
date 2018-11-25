b6954596bf80ca297d78a1ae4f7791e3dda4b436
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-11-25 14:02:28 -0800

    added css to backdrop and image

--- src/component/ImageComponent.jsx
+++ src/component/ImageComponent.jsx
@@ -7,6 +7,10 @@ export default class ImageComponent extends React.Component {
     this.setState({ isOpen: !this.state.isOpen });
   };
   render() {
+    let styles = {
+  width: '80%',
+  height: '90%',
+};
     return (
       <div>
         <img
@@ -24,6 +28,7 @@ export default class ImageComponent extends React.Component {
               src={this.props.image}
               onClick={this.handleShowDialog}
               alt='dog'
+              style={styles}
             />
           </dialog>
         )}
